4210caa02182d31e6684ca9e9c990206510eb574
YuXuan928 <beat4947@gmail.com>
2025-05-27 15:44:49 +0800

    更新子模組內容

---
+++ exercise_0523.test/contact.php
@@ -0,0 +1,28 @@
+<?php
+$username = $_POST["username"];
+$password = $_POST["password"];
+?>
+
+
+<!DOCTYPE html>
+<html lang="zh-tw">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>表單結果</title>
+</head>
+
+<body bgcolor="#B0C4DE">
+
+    <?php
+    {
+        echo "<h2>$username  您好 !</h2>"."<br>";
+
+    } 
+    echo "歡迎來到我的購物商城"."<br>";
+?>
+
+</body>
+
+</html>
